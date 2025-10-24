c9573aca6b5811b4ff9bc30264a3e218a1887380
taddemillion <tedlamillionyou@gmail.com>
2025-10-24 10:09:17 +0300

    download cors policy and  add to the builder

--- Program.cs
+++ Program.cs
@@ -3,23 +3,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi(); // this is for minimal apis
-
-var app = builder.Build();
 //  configuring the cors policy
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("allowAllOrigin", policy =>
+    options.AddPolicy("AllowSpecificOrigins", policy =>
     {
-        policy.AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowAnyOrigin()
-        .AllowCredentials()
-        .WithOrigins(
-            "http://10.100.13.44:3004",
-            "https://10.100.13.44:3004",
-            "http://localhost:3004",
-            "http://172.16.239.172:3000"
-        );
+        policy.WithOrigins(
+                "http://10.100.13.44:3004",
+                "https://10.100.13.44:3004",
+                "http://localhost:3004",
+                "http://172.16.239.172:3000"
+                )
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
     });
 });
 builder.Services.AddControllers();
@@ -30,6 +27,8 @@ builder.Services.Configure<IISOptions>(options =>
     // configure for IIS Deployments and have many properties
 
 });
+var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
