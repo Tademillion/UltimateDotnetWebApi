031f1ef88bf6611a2dcf32478c1f89ad79ccd686
taddemillion <tedlamillionyou@gmail.com>
2025-10-29 12:06:04 +0300

    apply migration  and   some data to teh database

--- RepositoryContext.cs
+++ RepositoryContext.cs
@@ -6,6 +6,11 @@ public class RepositoryContext : DbContext
     : base(options)
     {
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+        modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+    }
     public DbSet<Company> Companies { get; set; }
     public DbSet<Employee> Employees { get; set; }
 }
