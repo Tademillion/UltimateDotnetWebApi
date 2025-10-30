fd74cb1a57273873cb2759f39e61a86f1321a4cb
taddemillion <tedlamillionyou@gmail.com>
2025-10-30 08:35:58 +0300

    take notes for the  migrations

--- RepositoryContext.cs
+++ RepositoryContext.cs
@@ -13,4 +13,13 @@ public class RepositoryContext : DbContext
     }
     public DbSet<Company> Companies { get; set; }
     public DbSet<Employee> Employees { get; set; }
+    //  creating migration sin dotnet core 
+    //  install EF core for Dbcontext 
+    //  creating models for  our tables 
+    //  the craate dbcontext class for our models 
+    //  then install the ef.design for acces  the migration providers
+    //  and install the sqlServer for acces the sqlserver providers 
+    //  then  add the dbcontext service in program.cs for sake of registry
+    //  then run dotnet ef migrations add  migrationName
+    //  dotnet database update 
 }
