0519a9db93bf1c821e390568e10c033b202df2cc
taddemillion <tedlamillionyou@gmail.com>
2025-11-04 17:02:40 +0300

    cretae company resources

--- Contracts/Company/CompaniesController.cs
+++ Contracts/Company/CompaniesController.cs
@@ -1,12 +1,13 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
+[Route("api/companies")]
 public class CompaniesController(IRepositoryManager repo, IMapper mapper) : ControllerBase
 {
     private readonly IRepositoryManager _repo = repo;
     private readonly IMapper _mapper = mapper;
 
-    [HttpGet("api/companies")]
+    [HttpGet]
     public async Task<ActionResult> getCompanies()
     {
         var companies = _repo.Company.GetAllCompanies(false);
@@ -23,7 +24,7 @@ public class CompaniesController(IRepositoryManager repo, IMapper mapper) : Cont
         return Ok(companiesDto);
 
     }
-    [HttpGet("api/companies/{id}")]
+    [HttpGet("{id}", Name = "id")]// this should adde in order to get  CreatedAtRoute with correct http response
     public async Task<ActionResult> getSingleCompany(Guid id)
     {
         var company = _repo.Company.GetCompany(id, false);
@@ -34,5 +35,16 @@ public class CompaniesController(IRepositoryManager repo, IMapper mapper) : Cont
             var companyDto = _mapper.Map<CompanyDto>(company);
             return Ok(companyDto);
         }
+    }
+    //  create the company
+    [HttpPost]
+    public async Task<ActionResult> createCompany([FromBody] CompanyForCreationDto company)
+    {
+        var companyEntity = _mapper.Map<Company>(company);// convert the company to Company
+        _repo.Company.CreateCompany(companyEntity);
+        _repo.Save();
+        var companyToReturn = _mapper.Map<CompanyDto>(companyEntity);
+
+        return CreatedAtRoute("id", new { id = companyToReturn.Id }, companyToReturn);
     }
 }
--- Contracts/Company/CompanyRepository.cs
+++ Contracts/Company/CompanyRepository.cs
@@ -7,12 +7,17 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
   : base(repositoryContext)
   {
   }
+
+
   //  we can acces the RepositoryBase method in this class
   public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
-   FindAll(trackChanges)
-     .OrderBy(c => c.Name)
-     .ToList();
+ FindAll(trackChanges)
+   .OrderBy(c => c.Name)
+   .ToList();
 
   public Company GetCompany(Guid companyId, bool trackChanges) =>
      FindByCondition(m => m.Id.Equals(companyId), trackChanges).SingleOrDefault();
+  public void CreateCompany(Company company) => Create(company);// 
+
+
 }
