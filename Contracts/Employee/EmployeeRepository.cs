8e522ca639c865483fd9e64424448fa4ccce15c8
taddemillion <tedlamillionyou@gmail.com>
2025-11-19 08:39:05 +0300

    implement the search  Iquerables extension in the repo

--- Contracts/Employee/EmployeeRepository.cs
+++ Contracts/Employee/EmployeeRepository.cs
@@ -13,6 +13,7 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     var employees = await FindByCondition(e => e.CompanyId.Equals(companyId) &&
     e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge,
  trackChanges).FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+ .Search(employeeParameters.searchTerm)
  .OrderBy(e => e.Name)
  .ToListAsync();
     return PagedList<Employee>
--- Extensions/RepositoryEmployeeExtensions.cs
+++ Extensions/RepositoryEmployeeExtensions.cs
@@ -8,17 +8,8 @@ public static class RepositoryEmployeeExtensions {
    if(string.IsNullOrWhiteSpace(searchTerm))
    return employees;
    var lowerCaseTerm = searchTerm.Trim().ToLower();
+  return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
 
- return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+} 
 
-} // this is for searching the employees by name
-//  public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
-//  {
-//   if (string.IsNullOrWhiteSpace(orderByQueryString))
-//   return employees.OrderBy(e => e.Name);
-//   var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
-//   return employees.OrderBy(orderQuery);
-//  }
-//  // this is for sorting the employees by name
- 
    }
