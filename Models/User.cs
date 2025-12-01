bca04d5a76cba117694a8dfe48e3054475d075b1
taddemillion <tedlamillionyou@gmail.com>
2025-12-01 15:23:17 +0300

    update the identity users tables

--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,6 @@ using Microsoft.AspNetCore.Identity;
 public class User:IdentityUser
 {
 //  this means the User class inherits from IdentityUser, which includes properties like UserName and Email by default  
-    public string UserName { get; set; }
-    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
